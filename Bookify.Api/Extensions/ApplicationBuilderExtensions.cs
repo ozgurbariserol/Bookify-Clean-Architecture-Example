49438b1d3cb5275557c3a47d36163079037dc15c
Ozgur Baris Erol <ozgurbariserol34@gmail.com>
2024-05-23 23:52:41 +0300

    PresentationLayer added.

---
+++ Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Bookify.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions 
+    {
+        public static void ApplyMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+
+
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -4,6 +4,7 @@
     {
         private readonly List<IDomainEvent> domainEvents = new();
 
+        protected Entity() { }
 
         protected Entity(Guid id)
         {
